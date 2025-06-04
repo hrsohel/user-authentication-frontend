8db4cea0a76f21a3509a9472beca21049a957983
HR Sohel <hrsohel679@gmail.com>
2025-06-04 06:47:17 +0600

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { getSubdomain } from '../helper';
 import './App.css'
+import Home from './components/Home';
 import Login from './components/Login'
 import Profile from './components/Profile'
 import ShopPage from './components/ShopPage';
@@ -14,6 +15,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
