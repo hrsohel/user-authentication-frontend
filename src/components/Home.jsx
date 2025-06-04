eb8d9eb1c7e067ce3a558b831299d0bd95609d89
HR Sohel <hrsohel679@gmail.com>
2025-06-04 06:57:53 +0600

    navbar added on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+        <Navbar />
+        <h1>Home</h1>
+    </>
   )
 }
